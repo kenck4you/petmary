033c206e3399fe08eb8dec5ba3d4f3ebbc0a955d
Pfizer <lilkakaka224@gmail.com>
2026-06-30 17:14:17 +0700

    Remove db.sqlite3 from repo and create Pet model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from .services import generate_random_id
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owners = models.ManyToManyField(User)
+
+    GENDER_CHOICES = [
+        ('M', 'MALE'),
+        ('F', 'FEMALE'),
+        ('O', 'OTHER'),
+        ('N', 'NOT SPECIFIED'),
+    ]
+    
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default='N',
+        verbose_name='Gender'
+    )
+
+    def __str__(self):
+        return f"{self.name} (id={self.id})"
+
